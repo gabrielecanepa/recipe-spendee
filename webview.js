b5162909bcd3d80f0a563938cca8d46623b7f109
Gabriele Canepa <gabriele@canepa.io>
2018-04-10 12:40:59 +0100

    Add rules to remove some DOM elements

--- webview.js
+++ webview.js
@@ -1,5 +1,6 @@
-"use strict";
-
 module.exports = Franz => {
-  document.querySelector("._1EbB").style.display = "none";
+  document.querySelector("._1EbB").remove(); // Cookies bar
+  document.querySelectorAll("g")[4].remove(); // Beta logo
+  document.querySelector("._6NCS").remove(); // Membership logo
+  document.querySelectorAll(".navItem")[1].remove(); // Redeem list
 };
