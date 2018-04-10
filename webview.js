6c867112a5de4463cf9c9a645805b7dfc3afe755
Gabriele Canepa <gabriele@canepa.io>
2018-04-10 13:00:33 +0100

    Remove unuseful DOM rules

--- webview.js
+++ webview.js
@@ -1,6 +1,5 @@
+"use strict";
+
 module.exports = Franz => {
-  document.querySelector("._1EbB").remove(); // Cookies bar
-  document.querySelectorAll("g")[4].remove(); // Beta logo
-  document.querySelector("._6NCS").remove(); // Membership logo
-  document.querySelectorAll(".navItem")[1].remove(); // Redeem list
+  document.querySelector("._1EbB").remove(); // Removes cookies bar
 };
